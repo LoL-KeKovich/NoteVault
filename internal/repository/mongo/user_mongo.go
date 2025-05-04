1fde9588a4d69dca8b17f47a1478e59d0d3189ac
LoL-KeKovich <120039879+LoL-KeKovich@users.noreply.github.com>
2025-05-05 01:28:24 +0300

    init database operations

---
+++ internal/repository/mongo/user_mongo.go
@@ -0,0 +1,23 @@
+package mongo
+
+import "github.com/LoL-KeKovich/NoteVault/internal/model"
+
+func (mc MongoClient) CreateNote(model.Note) (string, error) {
+	return "", nil
+}
+
+func (mc MongoClient) GetNoteByID(id string) (model.Note, error) {
+	return model.Note{}, nil
+}
+
+func (mc MongoClient) GetNotes() ([]model.Note, error) {
+	return []model.Note{}, nil
+}
+
+func (mc MongoClient) UpdateNote(id string, name string, text string, color string, media string, order int) (int, error) {
+	return 0, nil
+}
+
+func (mc MongoClient) DeleteNote(id string) (int, error) {
+	return 0, nil
+}
