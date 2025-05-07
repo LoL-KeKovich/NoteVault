f8d2050dcb91ec129ebfe3d99bc3a2d2f799bae0
LoL-KeKovich <120039879+LoL-KeKovich@users.noreply.github.com>
2025-05-07 18:47:37 +0300

    added createnote handler

--- internal/service/note_service.go
+++ internal/service/note_service.go
@@ -1,8 +1,12 @@
 package service
 
 import (
+	"encoding/json"
+	"log/slog"
 	"net/http"
 
+	"github.com/LoL-KeKovich/NoteVault/internal/dto"
+	"github.com/LoL-KeKovich/NoteVault/internal/model"
 	"github.com/LoL-KeKovich/NoteVault/internal/repository"
 )
 
@@ -11,7 +15,38 @@ type NoteService struct {
 }
 
 func (srv NoteService) CreateNote(w http.ResponseWriter, r *http.Request) {
+	resp := dto.NoteResponse{}
+	var noteReq dto.NoteRequest
 
+	err := json.NewDecoder(r.Body).Decode(&noteReq)
+	if err != nil {
+		slog.Error(err.Error())
+		resp.Error = "Wrong request"
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(resp)
+		return
+	}
+
+	note := model.Note{
+		Name:  noteReq.Name,
+		Text:  noteReq.Text,
+		Color: noteReq.Color,
+		Media: noteReq.Media,
+		Order: noteReq.Order,
+	}
+
+	res, err := srv.DBClient.CreateNote(note)
+	if err != nil {
+		slog.Error(err.Error())
+		resp.Error = "Error inserting note in db"
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(resp)
+		return
+	}
+
+	slog.Info("Created note", slog.String("_id", res))
+	resp.Data = res
+	json.NewEncoder(w).Encode(resp)
 }
 
 func (srv NoteService) GetNoteByID(w http.ResponseWriter, r *http.Request) {
