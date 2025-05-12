e32b689c69948fdab96db6fddedfb4ff2a1e7a6e
LoL-KeKovich <120039879+LoL-KeKovich@users.noreply.github.com>
2025-05-12 15:21:04 +0300

    created tagrepo interface

---
+++ internal/repository/tag_repo.go
@@ -0,0 +1,11 @@
+package repository
+
+import "github.com/LoL-KeKovich/NoteVault/internal/model"
+
+type TagRepo interface {
+	CreateTag(model.Tag) (string, error)
+	GetTagByID(string) (model.Tag, error)
+	GetTags() ([]model.Tag, error)
+	UpdateTag(string, string, string) (int, error)
+	DeleteTag(string) (int, error)
+}
