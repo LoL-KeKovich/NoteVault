db422eb6fd7f7f356a9d29e62e3995dfb993b2d2
LoL-KeKovich <120039879+LoL-KeKovich@users.noreply.github.com>
2025-05-13 01:43:09 +0300

    added GetNotesByTags func

--- internal/repository/mongodb/note_tag_mongo.go
+++ internal/repository/mongodb/note_tag_mongo.go
@@ -3,7 +3,9 @@ package mongodb
 import (
 	"context"
 	"fmt"
+	"log/slog"
 
+	"github.com/LoL-KeKovich/NoteVault/internal/model"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
@@ -50,4 +52,43 @@ func (mc MongoClient) RemoveTagFromNote(noteID, tagID string) (int, error) {
 	}
 
 	return int(res.ModifiedCount), nil
+}
+
+func (mc MongoClient) GetNotesByTags(tagIDs []string) ([]model.Note, error) {
+	var objTagIDs []primitive.ObjectID
+
+	for _, id := range tagIDs {
+		objID, err := primitive.ObjectIDFromHex(id)
+		if err != nil {
+			return nil, fmt.Errorf("invalid tag id: %s", id)
+		}
+		objTagIDs = append(objTagIDs, objID)
+	}
+
+	filter := bson.D{{Key: "tags", Value: bson.D{{Key: "$all", Value: objTagIDs}}}}
+	cursor, err := mc.Client.Find(context.Background(), filter)
+	if err != nil {
+		return []model.Note{}, fmt.Errorf("error finding notes by tags: %v", err)
+	}
+	defer cursor.Close(context.Background())
+
+	var notes []model.Note
+
+	for cursor.Next(context.Background()) {
+		var note model.Note
+
+		err := cursor.Decode(&note)
+		if err != nil {
+			slog.Error("error decoding notes", slog.String("error", err.Error()))
+			continue
+		}
+
+		notes = append(notes, note)
+	}
+
+	if len(notes) == 0 {
+		return []model.Note{}, fmt.Errorf("no notes found with the specified tags")
+	}
+
+	return notes, nil
 }
