e9a501d82b7a80936320ad22d34b644fbf975729
LoL-KeKovich <120039879+LoL-KeKovich@users.noreply.github.com>
2025-05-26 21:29:37 +0300

    Created user struct

--- internal/model/user.go
+++ internal/model/user.go
@@ -1,1 +1,11 @@
 package model
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+type User struct {
+	ID           primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
+	PasswordHash string             `bson:"password_hash,omitempty" json:"-"`
+	Email        string             `bson:"email,omitempty" json:"email,omitempty"`
+	FirstName    string             `bson:"first_name,omitempty" json:"first_name,omitempty"`
+	LastName     string             `bson:"last_name,omitempty" json:"last_name,omitempty"`
+}
